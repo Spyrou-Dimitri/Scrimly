a698282a5973138a4da54eb7bc5ac5af8b793524
Spyrou-Dimitri <dimitri.spyrou@student.hepl.be>
2026-05-30 21:52:35 +0200

    Initiation du broadcasting(Laravel Reverb)

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,12 @@
 import './password-toggle.js';
 import { AvatarPreview } from './previewAvatar.js';
 import { WinrateChart } from './charts/winrate.js';
+/**
+ * Echo exposes an expressive API for subscribing to channels and listening
+ * for events that are broadcast by Laravel. Echo and event broadcasting
+ * allow your team to quickly build robust real-time web applications.
+ */
+import './echo';
 
 function initCalendarIfNeeded() {
     if (! document.getElementById('calendar')) {
@@ -15,3 +21,5 @@ WinrateChart.registerLivewireHooks();
 WinrateChart.scheduleRefresh();
 
 AvatarPreview.init();
+
+
