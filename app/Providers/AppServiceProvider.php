66f5044b2c6da05f872fb9385f4b3b691d66f025
Spyrou-Dimitri <dimitri.spyrou@student.hepl.be>
2026-06-07 00:22:34 +0200

    Passage du prohibitDestructivrCommand en false afin de pouvoir migrate:fresh

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -34,7 +34,7 @@ class AppServiceProvider extends ServiceProvider
         Date::use(CarbonImmutable::class);
 
         DB::prohibitDestructiveCommands(
-            app()->isProduction(),
+            false
         );
 
         Password::defaults(fn (): ?Password => app()->isProduction()
